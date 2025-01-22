0831bd5ac8ee0b14279488f4a4983dd9dd556bf7
flyingsl0ths <randomemail33110@protonmail.com>
2025-01-21 23:48:48 -0800

    Implement quit feature

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.ByteString as BS
+import qualified Data.Word as DW
 import qualified System.Posix.ByteString as PSB
 
+firstOf :: BS.ByteString -> Maybe DW.Word8
+firstOf = maybe Nothing (Just . fst) . BS.uncons
+
+charToWord8 :: Char -> DW.Word8
+charToWord8 = toEnum . fromEnum
+
 main :: IO ()
-main =
-  do
-    bs <- PSB.fdRead PSB.stdInput 1
-    print bs
-    main
+main = read'
+ where
+  read' =
+    do
+      bs <-
+        PSB.fdRead
+          PSB.stdInput
+          1
+      case firstOf bs of
+        Just q
+          | q == charToWord8 'q' -> return ()
+          | otherwise -> read'
+        _ -> read'
